7044424065de6dab21cfaba71e7829a8b765c8e3
ngoc99the <canhchjmkmoi@gmail.com>
2021-11-14 20:29:29 +0900

    thêm

---
+++ CAR_ORDER.sql
@@ -0,0 +1,27 @@
+create table CAR_ORDER (
+	OrderID INT,
+	CustomerID INT,
+	CarID INT,
+	Amount INT,
+	SalePrice VARCHAR(50),
+	OrderDate DATE,
+	DeliveryDate DATE,
+	DeliveryAdress VARCHAR(50),
+	Staus INT,
+	Note TEXT
+);
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (1, 1, 6, 2, '$27667.28', '2021/10/05', '2021/10/28', 'Hexia', 0, 'dapibus augue vel accumsan tellus nisi eu orci');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (2, 3, 3, 6, '$25982.92', '2021/10/07', '2021/10/26', 'Ardirejo', 1, 'orci luctus et ultrices posuere');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (3, 11, 2, 7, '$29589.14', '2021/10/21', '2021/10/28', 'Stockholm', 2, 'sollicitudin vitae consectetuer eget rutrum at');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (4, 15, 1, 5, '$26478.62', '2021/10/02', '2021/10/26', 'Cinangka', 0, 'lacus at velit vivamus vel nulla eget');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (5, 5, 10, 10, '$29735.95', '2021/10/30', '2021/10/25', 'Luoshui', 0, 'habitasse platea dictumst aliquam augue');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (6, 9, 2, 7, '$29344.90', '2021/10/23', '2021/10/27', 'Otradnoye', 1, 'habitasse platea dictumst aliquam augue');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (7, 1, 4, 6, '$26091.93', '2021/10/08', '2021/10/28', 'Jieheshi', 1, 'rutrum neque aenean auctor gravida sem praesent id massa id');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (8, 15, 5, 3, '$26587.10', '2021/10/29', '2021/10/26', 'Lashio', 2, 'fermentum justo nec condimentum neque sapien placerat ante');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (9, 11, 2, 6, '$27211.51', '2021/10/12', '2021/10/26', 'Pantanal', 1, 'neque aenean auctor gravida sem praesent id massa');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (10, 10, 13, 7, '$29588.41', '2021/10/04', '2021/10/28', 'Timrå', 0, 'nulla nunc purus phasellus in felis donec semper sapien a');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (11, 10, 6, 6, '$26932.23', '2021/10/18', '2021/10/27', 'Nanyaojie', 0, 'at dolor quis odio consequat varius');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (12, 3, 13, 7, '$29519.29', '2021/10/10', '2021/10/28', 'Jinzhou', 1, 'a suscipit nulla elit ac');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (13, 6, 2, 4, '$29521.81', '2021/10/28', '2021/10/25', 'Shangsanji', 1, 'in hac habitasse platea dictumst');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (14, 13, 12, 4, '$27574.99', '2021/10/27', '2021/10/26', 'Timbulsari', 1, 'nisl venenatis lacinia aenean sit amet justo');
+insert into CAR_ORDER (OrderID, CustomerID, CarID, Amount, SalePrice, OrderDate, DeliveryDate, DeliveryAdress, Staus, Note) values (15, 3, 8, 7, '$26486.93', '2021/10/02', '2021/10/25', 'Curuan', 0, 'fermentum donec ut mauris eget massa tempor convallis nulla');
